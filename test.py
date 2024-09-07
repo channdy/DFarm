8ef5831dce301274da704c0457d0dd6ce8ffb40f
channdy <keo.channdy@gmail.com>
2024-09-07 16:15:27 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,45 +1,44 @@
 import tkinter as tk
-from concurrent import futures
 import time
- 
-thread_pool_executor = futures.ThreadPoolExecutor(max_workers=5)
- 
-class MainFrame(tk.Frame):
- 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.label = tk.Label(self, text='not running')
-        self.label.pack()
-        self.listbox = tk.Listbox(self)
-        self.listbox.pack()
-        self.button = tk.Button(
-            self, text='blocking task', command=self.on_button)
-        self.button.pack(pady=15)
-        self.pack()
- 
-    def on_button(self):
-        print('Button clicked')
-        thread_pool_executor.submit(self.blocking_code)
- 
- 
-    def set_label_text(self, text=''):
-        self.label['text'] = text
- 
-    def listbox_insert(self, item):
-        self.listbox.insert(tk.END, item)
- 
-    def blocking_code(self):
-        self.after(0, self.set_label_text, 'running')
- 
-        for number in range(100):
-            self.after(0, self.listbox_insert, number)
-            print(number)
-            # time.sleep(1)
- 
-        self.after(0, self.set_label_text, ' not running')
- 
- 
-if __name__ == '__main__':
-    app = tk.Tk()
-    main_frame = MainFrame()
-    app.mainloop()
+import queue
+import threading
+def runloop(thread_queue=None):
+    result = 0
+    for i in range(1000):
+        pass
+        #Do something with result
+        time.sleep(5)
+    result = result + 1
+    thread_queue.put(result)
+class MainApp(tk.Tk):
+    def __init__(self):
+            ####### Do something ######
+            super(MainApp,self).__init__()
+            self.myframe = tk.Frame(self)
+            self.myframe.grid(row=0, column=0, sticky='nswe')
+            self.mylabel = tk.Label(self.
+            myframe) # Element to be updated
+            self.mylabel.config(text='No message')
+            self.mylabel.grid(row=0, column=0)
+            self.mybutton = tk.Button(
+            self.myframe,
+            text='Change message111',
+            command=self.update_text)
+            self.mybutton.grid(row=1, column=0)
+    def update_text(self):
+            self.mylabel.config(text='Running loop')
+            self.thread_queue = queue.Queue()
+            self.new_thread = threading.Thread(
+            target=runloop,
+            kwargs={'thread_queue':self.thread_queue})
+            self.new_thread.start()
+            self.after(100, self.listen_for_result)
+    def listen_for_result(self):
+            try:
+                self.res = self.thread_queue.get(0)
+                self.mylabel.config(text='Loop terminated')
+            except queue.Empty:
+                self.after(100, self.listen_for_result)
+if __name__ == "__main__":
+    main_app = MainApp()
+    main_app.mainloop()
